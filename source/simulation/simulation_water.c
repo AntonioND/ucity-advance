ab5a9330540d488503798c5e853d92e14b5b73d8
Antonio Niño Díaz <antonio_nd@outlook.com>
2021-05-04 18:53:10 +0100

    simulation: Implement water animation

---
+++ source/simulation/simulation_water.c
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0-only
+//
+// Copyright (c) 2021, Antonio Niño Díaz
+
+#include <stdlib.h>
+
+#include "room_game/draw_common.h"
+#include "room_game/room_game.h"
+#include "room_game/tileset_info.h"
+
+void Simulation_WaterAnimate(void)
+{
+    int count = (rand() & 31) + 1;
+
+    for (int j = 0; j < CITY_MAP_HEIGHT; j++)
+    {
+        for (int i = 0; i < CITY_MAP_WIDTH; i++)
+        {
+            if (count > 0)
+            {
+                count--;
+                continue;
+            }
+
+            count = (rand() & 31) + 1;
+
+            uint16_t tile = CityMapGetTile(i, j);
+            if (tile == T_WATER)
+                CityMapDrawTile(T_WATER_EXTRA, i, j);
+            else if (tile == T_WATER_EXTRA)
+                CityMapDrawTile(T_WATER, i, j);
+        }
+    }
+}
