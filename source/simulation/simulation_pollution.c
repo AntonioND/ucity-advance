8c9a9e3ba7318951866895c87fed28d422b4539f
Antonio Niño Díaz <antonio_nd@outlook.com>
2021-05-08 01:50:34 +0100

    simulation: Calculate pollution happiness flags

--- source/simulation/simulation_pollution.c
+++ source/simulation/simulation_pollution.c
@@ -16,6 +16,9 @@
 #include "simulation/simulation_pollution.h"
 #include "simulation/simulation_traffic.h"
 
+// Max valid pollution for zones that need non-polluted air
+#define POLLUTION_MAX_VALID_LEVEL   (256 / 2)
+
 EWRAM_BSS uint8_t scratch_map_1[CITY_MAP_WIDTH * CITY_MAP_HEIGHT];
 EWRAM_BSS uint8_t scratch_map_2[CITY_MAP_WIDTH * CITY_MAP_HEIGHT];
 
@@ -78,6 +81,71 @@ static void Diffuminate_Loop(uint8_t *src, uint8_t *dst)
     {
         for (int i = 0; i < CITY_MAP_WIDTH; i++)
             Diffuminate_Central_Tile(src, dst, i, j);
+    }
+}
+
+static void Simulation_PollutionSetTileOkFlag(void)
+{
+    // List of terrains that ignore the pollution level. In general, any terrain
+    // that generates pollution ignores it. This is only used for buildings, so
+    // no need to check fields, forests or water zones.
+    //
+    // This array says whether a particular tile type has to be checked for
+    // pollution or not. Type flags should be removed before accesing it.
+    //
+    // 1 = ignore this tile, 0 = handle pollution
+    const uint8_t ignore_tile_array[] = {
+        [TYPE_FIELD] = 1, // There's nothing here, don't check...
+        [TYPE_FOREST] = 1,
+        [TYPE_WATER] = 1,
+        [TYPE_RESIDENTIAL] = 0, // R and C must be clean. I generates pollution.
+        [TYPE_INDUSTRIAL] = 1,
+        [TYPE_COMMERCIAL] = 0,
+        [TYPE_POLICE_DEPT] = 1, // Services are supposed to work even in very
+        [TYPE_FIRE_DEPT] = 1,   //  polluted areas.
+        [TYPE_HOSPITAL] = 1,
+        [TYPE_PARK] = 0, // Recreation and education, they must be clean.
+        [TYPE_STADIUM] = 0,
+        [TYPE_SCHOOL] = 0,
+        [TYPE_HIGH_SCHOOL] = 0,
+        [TYPE_UNIVERSITY] = 0,
+        [TYPE_MUSEUM] = 0,
+        [TYPE_LIBRARY] = 0,
+        [TYPE_AIRPORT] = 1, // The tiles below generate pollution, so it's
+        [TYPE_PORT] = 1,    // illogical to ask for no pollution there.
+        [TYPE_DOCK] = 1,
+        [TYPE_POWER_PLANT] = 1,
+        [TYPE_FIRE] = 0, // Simulation should be off during fires.
+        [TYPE_RADIATION] = 1, // Ignore pollution here
+    };
+
+    for (int j = 0; j < CITY_MAP_HEIGHT; j++)
+    {
+        for (int i = 0; i < CITY_MAP_WIDTH; i++)
+        {
+            uint16_t type = CityMapGetType(i, j) & TYPE_MASK;
+
+            if (ignore_tile_array[type])
+            {
+                // This tile ignores pollution - Set "valid pollution level" bit
+                Simulation_HappinessSetFlags(i, j, TILE_OK_POLLUTION);
+            }
+            else
+            {
+                // Buildings require a level check...
+                unsigned int pollution = scratch_map_1[j * CITY_MAP_WIDTH + i];
+                if (pollution > POLLUTION_MAX_VALID_LEVEL)
+                {
+                    // Polluted
+                    Simulation_HappinessResetFlags(i, j, TILE_OK_POLLUTION);
+                }
+                else
+                {
+                    // Not polluted
+                    Simulation_HappinessSetFlags(i, j, TILE_OK_POLLUTION);
+                }
+            }
+        }
     }
 }
 
@@ -166,4 +234,9 @@ void Simulation_Pollution(void)
         // This message is shown only once per year
         // TODO : PersistentMessageShow(ID_MSG_POLLUTION_HIGH);
     }
+
+    // Set flags
+    // ---------
+
+    Simulation_PollutionSetTileOkFlag();
 }
