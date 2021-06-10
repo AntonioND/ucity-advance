07abc0774f27929d5ca25891e2fab1c934891d22
Antonio Niño Díaz <antonio_nd@outlook.com>
2021-06-10 01:24:51 +0100

    sfx: Prevent playing the same SFX more than once at a time

--- source/sfx.c
+++ source/sfx.c
@@ -10,30 +10,42 @@
 
 void SFX_Build(void)
 {
-    UMOD_SFX_Play(SFX_BUILD_WAV, UMOD_LOOP_DEFAULT);
+    static umod_handle handle = UMOD_HANDLE_INVALID;
+    UMOD_SFX_Stop(handle);
+    handle = UMOD_SFX_Play(SFX_BUILD_WAV, UMOD_LOOP_DEFAULT);
 }
 
 void SFX_BuildError(void)
 {
-    UMOD_SFX_Play(SFX_BUILD_ERROR_WAV, UMOD_LOOP_DEFAULT);
+    static umod_handle handle = UMOD_HANDLE_INVALID;
+    UMOD_SFX_Stop(handle);
+    handle = UMOD_SFX_Play(SFX_BUILD_ERROR_WAV, UMOD_LOOP_DEFAULT);
 }
 
 void SFX_Clear(void)
 {
-    UMOD_SFX_Play(SFX_CLEAR_WAV, UMOD_LOOP_DEFAULT);
+    static umod_handle handle = UMOD_HANDLE_INVALID;
+    UMOD_SFX_Stop(handle);
+    handle = UMOD_SFX_Play(SFX_CLEAR_WAV, UMOD_LOOP_DEFAULT);
 }
 
 void SFX_Demolish(void)
 {
-    UMOD_SFX_Play(SFX_DEMOLISH_WAV, UMOD_LOOP_DEFAULT);
+    static umod_handle handle = UMOD_HANDLE_INVALID;
+    UMOD_SFX_Stop(handle);
+    handle = UMOD_SFX_Play(SFX_DEMOLISH_WAV, UMOD_LOOP_DEFAULT);
 }
 
 void SFX_FireExplosion(void)
 {
-    UMOD_SFX_Play(SFX_FIRE_EXPLOSION_WAV, UMOD_LOOP_DEFAULT);
+    static umod_handle handle = UMOD_HANDLE_INVALID;
+    UMOD_SFX_Stop(handle);
+    handle = UMOD_SFX_Play(SFX_FIRE_EXPLOSION_WAV, UMOD_LOOP_DEFAULT);
 }
 
 void SFX_WrongSelection(void)
 {
-    UMOD_SFX_Play(SFX_WRONG_SELECTION_WAV, UMOD_LOOP_DEFAULT);
+    static umod_handle handle = UMOD_HANDLE_INVALID;
+    UMOD_SFX_Stop(handle);
+    handle = UMOD_SFX_Play(SFX_WRONG_SELECTION_WAV, UMOD_LOOP_DEFAULT);
 }
