8586821f5c1e77de2c8f4bfd30a56b507523c7ac
Antonio Niño Díaz <antonio_nd@outlook.com>
2021-07-05 01:10:20 +0100

    main: Add music information to jukebox

--- source/jukebox.c
+++ source/jukebox.c
@@ -7,18 +7,36 @@
 #include "audio.h"
 #include "jukebox.h"
 #include "random.h"
+#include "room_game/room_game.h"
+#include "simulation/calculate_stats.h"
 
 static jukebox_room current_room = JUKEBOX_ROOM_NONE;
 
 typedef struct {
-    int is_gbc;
+    int is_gba;
     jukebox_room room;
+    int city_level;
 } jukebox_song_info;
 
 static const jukebox_song_info song_info[] = {
-    [SONG_CITY_MOD]     = { 1, JUKEBOX_ROOM_GAME },
-    [SONG_MENU_MOD]     = { 1, JUKEBOX_ROOM_MAIN_MENU },
-    [SONG_TITLE_MOD]    = { 1, JUKEBOX_ROOM_INTRO },
+
+    // GBC songs
+
+    [SONG_TITLE_MOD]            = { 0, JUKEBOX_ROOM_INTRO, -1 },
+    [SONG_MENU_MOD]             = { 0, JUKEBOX_ROOM_MAIN_MENU, -1 },
+    [SONG_CITY_MOD]             = { 0, JUKEBOX_ROOM_GAME, -1 },
+
+    // GBA songs
+
+    [SONG_NO_ENERGY_MOD]        = { 1, JUKEBOX_ROOM_GAME, CLASS_VILLAGE },
+    [SONG_BALTHASAR_MOD]        = { 1, JUKEBOX_ROOM_GAME, CLASS_TOWN },
+    [SONG_THE_MSSING_LNK_MOD]   = { 1, JUKEBOX_ROOM_GAME, CLASS_CITY },
+    [SONG_JOINT_PEOPLE_MOD]     = { 1, JUKEBOX_ROOM_GAME, CLASS_METROPOLIS },
+    [SONG_IMPROV_MOD]           = { 1, JUKEBOX_ROOM_GAME, CLASS_CAPITAL },
+
+    [SONG_TECJAZZ_MOD]          = { 1, JUKEBOX_ROOM_INTRO, -1 },
+
+    [SONG_SCHWING_MOD]          = { 1, JUKEBOX_ROOM_MAIN_MENU, -1 },
 };
 
 static int current_index = 0;
@@ -28,13 +46,26 @@ void Jukebox_RoomSet(jukebox_room room)
     current_room = room;
 
     const size_t nelem = sizeof(song_info) / sizeof(jukebox_song_info);
+    int is_gba = Room_Game_Graphics_New_Get();
+    int city_level = Simulation_GetCityClass();
 
     uint32_t song_count = 0;
 
     for (size_t i = 0; i < nelem; i++)
     {
-        if (song_info[i].room == room)
-            song_count++;
+        if (song_info[i].is_gba != is_gba)
+            continue;
+
+        if (song_info[i].room != room)
+            continue;
+
+        if (song_info[i].city_level != -1)
+        {
+            if (song_info[i].city_level != city_level)
+                continue;
+        }
+
+        song_count++;
     }
 
     if (song_count == 0)
@@ -44,15 +75,24 @@ void Jukebox_RoomSet(jukebox_room room)
 
     for (size_t i = 0; i < nelem; i++)
     {
-        if (song_info[i].room == room)
+        if (song_info[i].is_gba != is_gba)
+            continue;
+
+        if (song_info[i].room != room)
+            continue;
+
+        if (song_info[i].city_level != -1)
         {
-            if (selection == 0)
-            {
-                current_index = i;
-                break;
-            }
-            selection--;
+            if (song_info[i].city_level != city_level)
+                continue;
         }
+
+        if (selection == 0)
+        {
+            current_index = i;
+            break;
+        }
+        selection--;
     }
 
     Audio_Song_Play(current_index);
@@ -67,16 +107,27 @@ void Jukebox_Update(void)
         return;
 
     const size_t nelem = sizeof(song_info) / sizeof(jukebox_song_info);
+    int is_gba = Room_Game_Graphics_New_Get();
+    int city_level = Simulation_GetCityClass();
 
     for (size_t i = 0; i < nelem; i++)
     {
         size_t check_index = (i + current_index) % nelem;
 
-        if (song_info[check_index].room == current_room)
+        if (song_info[i].is_gba != is_gba)
+            continue;
+
+        if (song_info[i].room != current_room)
+            continue;
+
+        if (song_info[i].city_level != -1)
         {
-            current_index = check_index;
-            Audio_Song_Play(current_index);
-            break;
+            if (song_info[i].city_level != city_level)
+                continue;
         }
+
+        current_index = check_index;
+        Audio_Song_Play(current_index);
+        break;
     }
 }
