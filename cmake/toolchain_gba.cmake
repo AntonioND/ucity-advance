6e8d645804fde4ba4d0c1679b825171cccea9294
Antonio Niño Díaz <antonio_nd@outlook.com>
2022-01-11 01:57:13 +0000

    cmake: Support building without devkitARM

--- cmake/toolchain_gba.cmake
+++ cmake/toolchain_gba.cmake
@@ -1,23 +1,22 @@
 # SPDX-License-Identifier: GPL-3.0-only
 #
-# Copyright (c) 2021 Antonio Niño Díaz
+# Copyright (c) 2021-2022 Antonio Niño Díaz
 
-if(DEFINED ENV{DEVKITARM})
-    set(DEVKITARM_BIN "$ENV{DEVKITARM}/bin/")
+if(USE_DEVKITARM)
+    if(DEFINED ENV{DEVKITARM})
+        set(ARM_GCC_PATH "$ENV{DEVKITARM}/bin/")
+    else()
+        set(ARM_GCC_PATH "/opt/devkitpro/devkitARM/bin/")
+    endif()
 else()
-    set(DEVKITARM_BIN "/opt/devkitpro/devkitARM/bin/")
+    set(ARM_GCC_PATH "")
 endif()
 
-if(DEFINED ENV{DEVKITPRO})
-    set(DEVKITPRO_TOOLS "$ENV{DEVKITPRO}/tools/bin/")
-else()
-    set(DEVKITPRO_TOOLS "/opt/devkitpro/tools/bin/")
-endif()
-
-set(CMAKE_ASM_COMPILER "${DEVKITARM_BIN}arm-none-eabi-gcc")
-set(CMAKE_C_COMPILER "${DEVKITARM_BIN}arm-none-eabi-gcc")
-set(CMAKE_OBJCOPY "${DEVKITARM_BIN}arm-none-eabi-objcopy")
-set(CMAKE_LINKER "${DEVKITARM_BIN}arm-none-eabi-ld")
+set(CMAKE_ASM_COMPILER "${ARM_GCC_PATH}arm-none-eabi-gcc")
+set(CMAKE_C_COMPILER "${ARM_GCC_PATH}arm-none-eabi-gcc")
+set(CMAKE_LINKER "${ARM_GCC_PATH}arm-none-eabi-ld")
+set(CMAKE_AR "${ARM_GCC_PATH}arm-none-eabi-ar")
+set(CMAKE_OBJCOPY "${ARM_GCC_PATH}arm-none-eabi-objcopy")
 
 set(CMAKE_SYSTEM_NAME "Game Boy Advance")
 set(CMAKE_SYSTEM_PROCESSOR ARM)
