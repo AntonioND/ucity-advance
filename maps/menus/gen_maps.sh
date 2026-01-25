887e3f17f858249c7438af97b64b10b257553967
Antonio Niño Díaz <antonio_nd@proton.me>
2026-01-25 10:52:48 +0000

    build: Update name of binary used in gen_maps.sh script

--- maps/menus/gen_maps.sh
+++ maps/menus/gen_maps.sh
@@ -12,15 +12,15 @@ echo ""
 echo "[*] Generating maps in ${IN}..."
 echo ""
 
-tiled.tmxrasterizer ${IN}/bank_offer_menu_bg.tmx ${IN}/bank_offer_menu_bg.png
-tiled.tmxrasterizer ${IN}/bank_repay_menu_bg.tmx ${IN}/bank_repay_menu_bg.png
-tiled.tmxrasterizer ${IN}/budget_menu_bg.tmx ${IN}/budget_menu_bg.png
-tiled.tmxrasterizer ${IN}/city_stats_bg.tmx ${IN}/city_stats_bg.png
-tiled.tmxrasterizer ${IN}/credits_bg.tmx ${IN}/credits_bg.png
-tiled.tmxrasterizer ${IN}/generate_map_bg.tmx ${IN}/generate_map_bg.png
-tiled.tmxrasterizer ${IN}/graphs_frame_bg.tmx ${IN}/graphs_frame_bg.png
-tiled.tmxrasterizer ${IN}/main_menu_bg.tmx ${IN}/main_menu_bg.png
-tiled.tmxrasterizer ${IN}/minimap_frame_bg.tmx ${IN}/minimap_frame_bg.png
-tiled.tmxrasterizer ${IN}/name_input_menu_bg.tmx ${IN}/name_input_menu_bg.png
-tiled.tmxrasterizer ${IN}/save_menu_bg.tmx ${IN}/save_menu_bg.png
-tiled.tmxrasterizer ${IN}/scenario_selection_bg.tmx ${IN}/scenario_selection_bg.png
+tmxrasterizer ${IN}/bank_offer_menu_bg.tmx ${IN}/bank_offer_menu_bg.png
+tmxrasterizer ${IN}/bank_repay_menu_bg.tmx ${IN}/bank_repay_menu_bg.png
+tmxrasterizer ${IN}/budget_menu_bg.tmx ${IN}/budget_menu_bg.png
+tmxrasterizer ${IN}/city_stats_bg.tmx ${IN}/city_stats_bg.png
+tmxrasterizer ${IN}/credits_bg.tmx ${IN}/credits_bg.png
+tmxrasterizer ${IN}/generate_map_bg.tmx ${IN}/generate_map_bg.png
+tmxrasterizer ${IN}/graphs_frame_bg.tmx ${IN}/graphs_frame_bg.png
+tmxrasterizer ${IN}/main_menu_bg.tmx ${IN}/main_menu_bg.png
+tmxrasterizer ${IN}/minimap_frame_bg.tmx ${IN}/minimap_frame_bg.png
+tmxrasterizer ${IN}/name_input_menu_bg.tmx ${IN}/name_input_menu_bg.png
+tmxrasterizer ${IN}/save_menu_bg.tmx ${IN}/save_menu_bg.png
+tmxrasterizer ${IN}/scenario_selection_bg.tmx ${IN}/scenario_selection_bg.png
